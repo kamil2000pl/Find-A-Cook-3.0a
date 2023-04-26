7e46e017da3ca10c509ffe14cc9d74e22ef66d45
ConorMcGuire <conor@cjmcguire.net>
2023-04-26 23:57:32 +0100

    switched deployed backend to server.js for some calls

--- main/findacook-app/src/components/AdminCookDisplay/AdminCookDisplay.js
+++ main/findacook-app/src/components/AdminCookDisplay/AdminCookDisplay.js
@@ -12,7 +12,7 @@ const AdminCookDisplay = () => {
   
     const fetchCooks = async () => {
       try {
-        const response = await axios.get("https://findacook-web.onrender.com/api/cook/allcooks");
+        const response = await axios.get("https://findacook-server.onrender.com/api/cook/allcooks");
         setCooks(response.data.cooks);
       } catch (error) {
         console.error("Error fetching cooks:", error);
